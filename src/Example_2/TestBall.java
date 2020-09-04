0cb969735b062c19cee069f7adbf1b3c99af3bb1
Зефирка <loikoanton@yandex.ru>
2020-09-04 10:40:38 +0300

    :D

--- src/Example_2/TestBall.java
+++ src/Example_2/TestBall.java
@@ -1,4 +1,16 @@
 package Example_2;
 
+import javax.sound.midi.SysexMessage;
+
 public class TestBall {
+    public static void main(String[] args){
+        Ball b1 = new Ball(100,100);
+        Ball b2 = new Ball(150,200);
+        System.out.println(b1);
+        System.out.println(b2);
+        b1.move(30,15);
+        b2.move(40,10);
+        System.out.println(b1);
+        System.out.println(b2);
+    }
 }
